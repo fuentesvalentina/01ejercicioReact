ea46620e810c3960ba6b720dc5d72c8837a4eae7
Valentina Fuentes <fuentesvalentina74@gmail.com>
2024-02-05 23:46:28 -0300

    agrego ejercicio numero 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css"
 
+function App() {
+//aqui agrego la logica del componente, dentro de la funcion pero antes del return
   return (
+    //aqui puedo escribir un poquito mas de logica
     <>
+    {/* aq */}
+      <h1 className="text-center">Hello world!</h1>
     </>
   )
 }
